a13d988c3ca5513a5c2658a5c2f1c102196797c1
Todd Demone <tdemone1@gmail.com>
2024-09-03 17:47:15 -0400

    add ignores files list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,25 @@ import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  {
+    ignores: [
+      'dist/**/*.ts',
+      'dist/**',
+      "**/*.mjs",
+      "eslint.config.mjs",
+      "**/*.js"
+    ],
+  },
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   prettierConfig,
-  // ...tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
