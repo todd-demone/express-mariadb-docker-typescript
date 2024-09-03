e79500981e34e5796ddcceb4333a1a411719db3d
Todd Demone <tdemone1@gmail.com>
2024-09-02 22:55:45 -0400

    import db connection and make query; import express Types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { RowDataPacket } from 'mysql2/promise';
+import conn from './db';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express-MariaDB-Docker-TypeScript!')
-});
+app.get('/', async (req: Request, res: Response) => {
+    const [rows, fields] = await conn.query<RowDataPacket[]>('SELECT * FROM `user`');
+    res.send(`Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 });
