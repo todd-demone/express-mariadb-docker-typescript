338f01d31d03dad013a6d79f8e7a3418b605b161
Todd Demone <tdemone1@gmail.com>
2024-09-03 17:47:59 -0400

    get rid of try catch, next(). clean up sql query; add eslint ignore rule

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // index.ts
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { RowDataPacket } from "mysql2/promise";
@@ -8,20 +8,17 @@ import conn from "./db";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(morgan("combined"));
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>("SELECT * FROM `user`");
-    res.send(
-      `Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`
-    );
-    console.log("DONE");
-  } catch (err) {
-    next(err);
-  }
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.get("/", async (_req: Request, res: Response) => {
+  const sqlQuery = "SELECT * FROM `user`";
+  const [rows] = await conn.query<RowDataPacket[]>(sqlQuery);
+  res.send(
+    `Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`
+  );
 });
 
 app.listen(PORT, () => {
