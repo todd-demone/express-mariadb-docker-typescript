3b27256213abcd5bdce7c342d19ca7747891abca
Todd Demone <tdemone1@gmail.com>
2024-09-03 09:38:24 -0400

    add next function to request handler function so that errors can be handled by Express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import { RowDataPacket } from 'mysql2/promise';
 import conn from './db';
@@ -8,12 +8,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const [rows] = await conn.query<RowDataPacket[]>('SELECT * FROM `user`');
         res.send(`Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`);
+        console.log("DONE")
     } catch (err) {
-        console.log("ERROR: ", err);
+        next(err);
     }
   });
 
