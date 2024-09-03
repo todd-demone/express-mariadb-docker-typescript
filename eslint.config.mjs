69e1f20b59b6ceba65fb488cca9a305984d2b609
Todd Demone <tdemone1@gmail.com>
2024-09-03 15:31:20 -0400

    make tseslint config strict and stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,6 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
 );
