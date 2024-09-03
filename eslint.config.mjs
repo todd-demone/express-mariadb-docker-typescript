1dc41a426626d08cf397018417130472cf1c292e
Todd Demone <tdemone1@gmail.com>
2024-09-03 16:49:52 -0400

    eslint and prettier changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  prettierConfig,
+  // ...tseslint.configs.stylistic,
 );
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import { RowDataPacket } from 'mysql2/promise';
-import conn from './db';
+// index.ts
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { RowDataPacket } from "mysql2/promise";
+import conn from "./db";
 
 dotenv.config();
 
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("combined"));
 
-app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const [rows] = await conn.query<RowDataPacket[]>('SELECT * FROM `user`');
-        res.send(`Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`);
-        console.log("DONE")
-    } catch (err) {
-        next(err);
-    }
-  });
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const [rows] = await conn.query<RowDataPacket[]>("SELECT * FROM `user`");
+    res.send(
+      `Hello, ${rows[0].name}. Welcome to Express-mariadb-docker-typescript!`
+    );
+    console.log("DONE");
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
