5020a184f4c7b738489a0c15810d6f6d08eecb19
Todd Demone <tdemone1@gmail.com>
2024-09-04 14:15:10 -0400

    rename db query function, remove comments

--- src/index.ts
+++ src/index.ts
@@ -16,23 +16,17 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(morgan("combined"));
 
-const asyncGetAllUsers = async function (pool: Pool): Promise<RowDataPacket[]> {
-  // removed try-catch here to avoid redundancy (two places - here and error middleware);
-  // instead of dealing with errors here, I delegated
-  // error handling to the wrapper callback function in app.get()
-  // (using async/await's `.catch` method - i.e., `.catch(next)`)
-  // and my errorHandler middleware
+const getAllUsers = async function (pool: Pool): Promise<RowDataPacket[]> {
   const sqlQuery = "SELECT * FROM `user`";
   const [rows] = await pool.query<RowDataPacket[]>(sqlQuery);
   return rows;
 };
 
-// Wrapping the async handler in a regular function
-// Goal: avoid returning a Promise from our async/await function;
+// Wrapping the async database query function in a regular function
+// Goal: avoid returning a Promise from our app.get() callback function;
 // instead we return void, which is what app.get() expects.
 app.get("/", (_req: Request, res: Response, next: NextFunction) => {
-  // If there are any unhandled promise rejections after calling this async function, they are caught and passed to next().
-  asyncGetAllUsers(pool)
+  getAllUsers(pool)
     .then((r: RowDataPacket[]) =>
       res.send(`Welcome, ${r[0].name}, to Express-TypeScript-MariaDB-Docker!`)
     )
